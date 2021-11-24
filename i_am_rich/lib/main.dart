e20a3f78811c75c1780d3778aff8f226f861f736
Joey <yty0606@gmail.com>
2021-11-24 11:58:42 +0800

    Feat: Scaffolding a flutter app
    Solution:
    1. As title. 
    2. Reference class 26.
    
    Author: Joey

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,8 +4,16 @@ void main() {
   //runApp(MaterialApp(home: Text('Hello world')));
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+        ),
+        body: Center( //Center也是一個 widget
+          child: Image(
+            image: NetworkImage('https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg'),  //字串只要用single quote
+          ),
+        ),
       ),
     ),
   );
