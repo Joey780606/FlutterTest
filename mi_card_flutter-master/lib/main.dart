ed1f0b8b99440106bf5962437e38d2bb4ce7ff49
Joey yang <joeyyang@pcpartner.com>
2021-12-04 07:30:16 +0800

    Feat: Tapping into widget properties
    Solution:
    1. As title. 
    2. Reference class 44.
    
    Author: Joey

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -19,45 +19,27 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-           //mainAxisSize: MainAxisSize.min,  //表示Column會在垂直方向占用儘可能少的空間(
-           //verticalDirection: VerticalDirection.up,  //表示會由下面往"上"長, down是default值
-           // mainAxisAlignment: MainAxisAlignment.spaceEvenly, //預設是start,最上面, 若end,是最下面, center, spaceEvenly,
-            //crossAxisAlignment: CrossAxisAlignment.stretch, //stretch:會把所有元件的width變成 double.infinity(無限長),這樣就不用每個Container都設 double.infinity 了
             children: <Widget>[
-              Container(
-              height: 100.0,
-              width: 30.0,
-              color: Colors.white,
-              child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50.0, //只有這個的話,就會畫一個圓形的radius
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/images001.jfif'),  //jfif 也可以用哦, jpeg file interchange format
               ),
-              SizedBox( //中間會產生空格
-                height: 20.0,
-                //width: 30.0,
-              ),
-              Container(
-                height: 100.0,
-                width: 150.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.red,
-                child: Text('Container 3'),
-              ),
-              Container(
-                width: double.infinity,
-                height: 10.0,
-              ),
+              Text(
+                  'Angelins Jolie',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
             ],
-          ),
+          )
         ),
       ),
     );
   }
 }
-
