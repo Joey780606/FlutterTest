3d6f30b6808a4b8f8732f7377e1d821f720aac92
Joey <yty0606@gmail.com>
2021-12-06 10:59:45 +0800

    Feat: Adding material icons with the icon widget
    Solution:
    1. As title.  Design telephone and email list.
    2. Reference class 46.
    
    Author: Joey

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -47,8 +47,55 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0), //這樣Container就會跟外面一層有間隔
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+886 1234 5678',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0
+                      )
+                    )],
+                ),
+
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0), //這樣Container就會跟外面一層有間隔
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                        'abc@d.e.f',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20.0
+                        )
+                    )],
+                ),
+              ),
             ],
-          )
+          ),
         ),
       ),
     );
