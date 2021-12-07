ec77b71eba27bb87edc668b9b6c5f941df734605
Joey <yty0606@gmail.com>
2021-12-07 10:38:00 +0800

    Feat: Using the expanded widget to create flexible layouts
    Solution:
    1. As title. 
    2. Reference class 51.
    3. Skill: StatelessWidget, Expanded > flex, image.asset 或 image.network
    
    Author: Joey

--- dicee-flutter-master/lib/main.dart
+++ dicee-flutter-master/lib/main.dart
@@ -18,6 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        //Image(  // 舊的方法
+        //  width: 200.0, //hardcode ,不建議
+        //  image: AssetImage('images/dice1.png'),
+        //),
+        Expanded( //要在 Row 和 Column 裡使用
+          //flex: 2, //每個row/column項目顯示的比例
+          //child: Image( //較舊的寫法,新的在下面
+          //  image: AssetImage('images/dice1.png'),
+          //),
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded( //要幾項就要用幾個
+          //flex: 1,
+          //child: Image(
+          //  image: AssetImage('images/dice1.png'),
+          //),
+          child: Image.asset('images/dice1.png'),
+        ),
+      ],
+    );
   }
 }
